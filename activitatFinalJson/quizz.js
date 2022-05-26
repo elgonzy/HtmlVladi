3d0f982c7ba9951e0a1e08c305db992d850208e1
elgonzy <210701Ag>
2022-05-26 15:49:53 +0200

    Share with vladi.

--- activitatFinalJson/quizz.js
+++ activitatFinalJson/quizz.js
@@ -1,116 +1,20 @@
-let quizJSON = {
-    "quiz": [
-        {
-
-            "question": "Which one is correct team name in NBA?",
-            "options": [
-                "New York Bulls",
-                "Los Angeles Kings",
-                "Golden State Warriros",
-                "Huston Rocket"
-            ],
-            "answer": "4"
-
-        },
-
-        {
-            "question": "5 + 7 = ?",
-            "options": [
-                "10",
-                "11",
-                "12",
-                "13"
-            ],
-            "answer": "3"
-        },
-        {
-            "question": "How to check an input from JS?",
-            "options": [
-                "input type='rounded' checked",
-                ".checked = true;",
-                "style:'checked'",
-                "semen en lata"
-            ],
-            "answer": "2"
-        },
-        {
-            "question": "Where Mitu was born?",
-            "options": [
-                "Ceuta",
-                "Melilla",
-                "Bucharest",
-                "GitanoLand"
-            ],
-            "answer": "3"
-        },
-        {
-            "question": "Where Cristian was born?",
-            "options": [
-                "Chile",
-                "Peru",
-                "Melilla",
-                "MoroLand"
-            ],
-            "answer": "1"
-        },
-        {
-
-            "question": "Which one is correct team name in Soccer?",
-            "options": [
-                "Barca",
-                "Rial Madrid",
-                "Diabetis",
-                "Atletico De Madrid"
-            ],
-            "answer": "4"
-
-        },
-        {
-            "question": "3 + 7 = ?",
-            "options": [
-                "10",
-                "11",
-                "12",
-                "13"
-            ],
-            "answer": "1"
-        },
-        {
-            "question": "How to use css correctly?",
-            "options": [
-                "input type='rounded' checked",
-                ".checked = true;",
-                "style='margin:40%' ",
-                "semen en lata"
-            ],
-            "answer": "3"
-        },
-        {
-            "question": "Where Pol was born?",
-            "options": [
-                "Ceuta",
-                "Barcelona",
-                "Bucharest",
-                "GitanoLand"
-            ],
-            "answer": "4"
-        },
-        {
-            "question": "Where Xavi was born?",
-            "options": [
-                "Sanseloni",
-                "Sansa",
-                "San Celoni",
-                "MoroLand"
-            ],
-            "answer": "3"
-        }
-    ]
-};
+const adresaURL = "https://quizapi.io/api/v1/questions?apiKey=Uwb3SuOcDjOWBDHHrjpdutmFKk2TOg2RqMljljFE&category=sql&difficulty=Medium&limit=10";
+const peticio = new XMLHttpRequest();
+var quizJSON = "";
+peticio.open('GET', adresaURL);
+peticio.responseType = 'json';
+peticio.send();
+peticio.onload = function () {
+    quizJSON = peticio.response;
+}
+console.log(quizJSON);
 
 let index = 0;
 let answer;
 let selectedAnswer = [];
+
+
+
 
 //#region index management
 
@@ -200,13 +104,13 @@ function reset() {
 function update() {
 
     console.log("S'han actualitzat les dades");
-    
-    document.getElementById("question").innerHTML = quizJSON.quiz[index].question;
-    
-    for (let i = 0; i < 4; i++)
-        document.getElementById("labelOption" + i).innerHTML = quizJSON.quiz[index].options[i];
 
-    for (let index = 0; index < 4; index++) {
+    document.getElementById("question").innerHTML = quizJSON[index].question;
+
+    for (let i = 0; i < 4; i++)
+        document.getElementById("labelOption" + i).innerHTML = quizJSON[index].options[i];
+
+    for (let i = 0; i < 4; i++) {
         if (selectedAnswer[index] == i) {
             document.getElementById("option" + i).checked = true;
 
