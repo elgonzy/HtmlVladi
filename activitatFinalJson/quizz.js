cb9222c8ca2195903e36dcd3709b30c6b198ea31
elgonzy <210701Ag>
2022-05-28 14:02:32 +0200

    entrega vladi

--- activitatFinalJson/quizz.js
+++ activitatFinalJson/quizz.js
@@ -1,26 +1,23 @@
-const adresaURL = "https://quizapi.io/api/v1/questions?apiKey=Uwb3SuOcDjOWBDHHrjpdutmFKk2TOg2RqMljljFE&category=sql&difficulty=Medium&limit=10";
+const adresaURL = "https://quizapi.io/api/v1/questions?apiKey=5GbZgPE1MaJkrJWHN3pi3ndckIZ5FSK2YwugrgBI&category=sql&difficulty=Medium&limit=10";
 const peticio = new XMLHttpRequest();
-var quizJSON = "";
+
 peticio.open('GET', adresaURL);
 peticio.responseType = 'json';
 peticio.send();
 peticio.onload = function () {
-    quizJSON = peticio.response;
+    console.log(peticio.response);
 }
-console.log(quizJSON);
 
 let index = 0;
 let answer;
 let selectedAnswer = [];
 
-
-
-
 //#region index management
+
 
 function increaseIndex() {
 
-    if (index < quizJSON.quiz.length - 1) {
+    if (index < peticio.response.length - 1) {
         index++;
     }
     update();
@@ -47,12 +44,14 @@ function resetIndex() {
 
 //#region quiz manager
 
-function getChecked() {
+const checkBox = [document.getElementById('option0').checked,
+document.getElementById('option1').checked,
+document.getElementById('option2').checked,
+document.getElementById('option3').checked,
+document.getElementById('option4').checked,
+document.getElementById('option5').checked];
 
-    const checkBox = [document.getElementById('option0').checked,
-    document.getElementById('option1').checked,
-    document.getElementById('option2').checked,
-    document.getElementById('option3').checked];
+function getChecked() {
 
     for (let i = 0; i < checkBox.length; i++) {
 
@@ -74,8 +73,8 @@ function correct() {
     for (let i = 0; i < selectedAnswer.length; i++) {
 
         console.log(selectedAnswer[i]);
-        console.log(quizJSON.quiz[i].answer);
-        if (selectedAnswer[i] + 1 == parseInt(quizJSON.quiz[i].answer)) {
+        console.log(peticio.response[i].answer);
+        if (selectedAnswer[i] + 1 == parseInt(peticio.response[i].answer)) {
             answer++;
         }
 
@@ -105,15 +104,24 @@ function update() {
 
     console.log("S'han actualitzat les dades");
 
-    document.getElementById("question").innerHTML = quizJSON[index].question;
+    document.getElementById("question").innerHTML = peticio.response[index].question;
 
-    for (let i = 0; i < 4; i++)
-        document.getElementById("labelOption" + i).innerHTML = quizJSON[index].options[i];
+    document.getElementById("labelOption0").innerHTML = peticio.response[index].answers.answer_a;
+    document.getElementById("labelOption1").innerHTML = peticio.response[index].answers.answer_b;
+    if (peticio.response[index].answers.answer_c != null) {
+        document.getElementById("labelOption2").innerHTML = peticio.response[index].answers.answer_c;
+        checkBox[2].style.display = none
 
-    for (let i = 0; i < 4; i++) {
+        
+    }
+
+    document.getElementById("labelOption3").innerHTML = peticio.response[index].answers.answer_d;
+    document.getElementById("labelOption4").innerHTML = peticio.response[index].answers.answer_e;
+    document.getElementById("labelOption5").innerHTML = peticio.response[index].answers.answer_f;
+
+    for (let i = 0; i < 6; i++) {
         if (selectedAnswer[index] == i) {
             document.getElementById("option" + i).checked = true;
-
         } else {
             document.getElementById("option" + i).checked = false;
 
