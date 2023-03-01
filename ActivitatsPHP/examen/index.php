7dc0d4109a8e9116ae579357c7a776c5ec1a7106
elgonzy <adriangonzalez2003@ginebro.cat>
2023-03-01 15:54:01 +0100

    me habia olvidado de que iba con fetch todo el tema

--- ActivitatsPHP/examen/index.php
+++ ActivitatsPHP/examen/index.php
@@ -8,28 +8,7 @@ require_once 'assets\PHP\Base.php';
     
     if ($base -> doLogin() || isset($_SESSION['username'])) {
         
-
-        
-
-        $html = "<!DOCTYPE html>
-        <html lang='en'>
-        <head>
-            <meta charset='UTF-8'>
-            <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-            <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-            <title>Document</title>
-        </head>
-        <body>
-            <header>
-                <nav>
-                    <a>".$_SESSION['username']."</a>
-                    <a href='./tancarsessio.php'>cerrar sesion</a>
-                </nav>
-            </header>
-        </body>
-        </html>";
-        
-        echo $html;
+        echo 
         
     }else {
 
