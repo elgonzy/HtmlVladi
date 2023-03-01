f83d96d35b6ea466fac4dfe6c25fab5eca787fdb
elgonzy <adriangonzalez2003@ginebro.cat>
2023-03-01 15:51:19 +0100

    working loggin

--- ActivitatsPHP/examen/index.php
+++ ActivitatsPHP/examen/index.php
@@ -1,16 +1,35 @@
 <?php
-
 namespace index;
 use assets\PHP\utils\Base;
 require_once 'assets\PHP\Base.php';
     
     $base = new Base; // Instanciamos la clase base donde tengo metodos para gestionar cosas base de la actividad
 
-    $html = "";
     
     if ($base -> doLogin() || isset($_SESSION['username'])) {
+        
 
-        $base -> test_connection();
+        
+
+        $html = "<!DOCTYPE html>
+        <html lang='en'>
+        <head>
+            <meta charset='UTF-8'>
+            <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+            <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+            <title>Document</title>
+        </head>
+        <body>
+            <header>
+                <nav>
+                    <a>".$_SESSION['username']."</a>
+                    <a href='./tancarsessio.php'>cerrar sesion</a>
+                </nav>
+            </header>
+        </body>
+        </html>";
+        
+        echo $html;
         
     }else {
 
