a9bf6bda00452810dde4dac7c04775860ebfee99
elgonzy <adriangonzalez2003@ginebro.cat>
2023-05-24 16:05:25 +0200

    muestra datos en web, no en el pdf

--- ActivitatsPHP/PRACTICAFINAL/consultar_gastos.php
+++ ActivitatsPHP/PRACTICAFINAL/consultar_gastos.php
@@ -1,16 +1,16 @@
 <?php
 session_start();
 
-// Verificar si el usuario ha iniciado sesi贸n
+// Verificar si el usuario ha iniciado sesi髇
 if (!isset($_SESSION['username'])) {
     header('Location: index.html');
     exit;
 }
 
-// Obtener el nombre de usuario de la sesi贸n
+// Obtener el nombre de usuario de la sesi髇
 $username = $_SESSION['username'];
 
-// Establecer conexi贸n con la base de datos
+// Establecer conexi髇 con la base de datos
 $conn = new mysqli('localhost', 'root', '', 'gestFin');
 if ($conn->connect_error) {
     die('Error al conectar a la base de datos: ' . $conn->connect_error);
@@ -30,9 +30,68 @@ if (isset($_POST['fecha_inicio']) && isset($_POST['fecha_fin'])) {
 }
 
 $result = $conn->query($sql);
-
-// Cerrar la conexi贸n a la base de datos
 $conn->close();
+
+// Generar el informe en formato PDF
+require('assets/fpdf/fpdf.php');
+
+class PDF extends FPDF
+{
+    // Cabecera de p醙ina personalizada
+    function Header()
+    {
+        // T韙ulo del informe
+        $this->SetFont('Arial', 'B', 14);
+        $this->Cell(0, 10, 'Informe de Gastos - Mes Actual', 0, 1, 'C');
+        $this->Ln(5);
+    }
+    
+    // Pie de p醙ina personalizado
+    function Footer()
+    {
+        // N鷐ero de p醙ina
+        $this->SetY(-15);
+        $this->SetFont('Arial', 'I', 8);
+        $this->Cell(0, 10, 'P醙ina ' . $this->PageNo() . ' / {nb}', 0, 0, 'C');
+    }
+}
+
+// Crear un nuevo PDF
+$pdf = new PDF();
+$pdf->AliasNbPages();
+$pdf->AddPage();
+
+// Agregar contenido al informe
+$pdf->SetFont('Arial', 'B', 12);
+$pdf->Cell(40, 10, 'ID', 1);
+$pdf->Cell(50, 10, 'Categor韆', 1);
+$pdf->Cell(40, 10, 'Monto', 1);
+$pdf->Cell(50, 10, 'Fecha', 1);
+$pdf->Ln();
+
+$pdf->SetFont('Arial', '', 10);
+
+
+// Cerrar la conexi髇 a la base de datos
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $pdf->Cell(40, 10, $row['id'], 1);
+        $pdf->Cell(50, 10, $row['categoria'], 1);
+        $pdf->Cell(40, 10, $row['monto'], 1);
+        $pdf->Cell(50, 10, $row['fecha'], 1);
+        $pdf->Ln();
+    }
+} else {
+    $pdf->Cell(180, 10, 'No se encontraron gastos para el mes actual.', 1, 1, 'C');
+}
+
+// Descargar el informe en formato PDF si se ha pulsado el bot髇 de descarga
+if (isset($_POST['download'])) {
+    $pdf->Output('Informe_Gastos_Mes_Actual.pdf', 'D');
+    exit;
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -53,31 +112,56 @@ $conn->close();
        <input type="date" name="fecha_fin" id="fecha_fin">
        
        <button type="submit">Filtrar</button>
+       <button type="submit" name="download">Descargar PDF</button>
    </form>
    
     <table>
         <tr>
             <th>ID</th>
-            <th>Categor铆a</th>
+            <th>Categor韆</th>
             <th>Monto</th>
             <th>Fecha</th>
         </tr>
         <?php
-        if ($result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
-                echo '<tr>';
-                echo '<td>' . $row['id'] . '</td>';
-                echo '<td>' . $row['categoria'] . '</td>';
-                echo '<td>' . $row['monto'] . '</td>';
-                echo '<td>' . $row['fecha'] . '</td>';
-                echo '</tr>';
+
+            // Establecer conexi髇 con la base de datos
+            $conn = new mysqli('localhost', 'root', '', 'gestFin');
+            if ($conn->connect_error) {
+                die('Error al conectar a la base de datos: ' . $conn->connect_error);
             }
-        } else {
-            echo '<tr><td colspan="4">No se encontraron gastos para el rango de fechas especificado.</td></tr>';
-        }
+
+            // Filtrar por fechas si se han enviado los campos del formulario
+            if (isset($_POST['fecha_inicio']) && isset($_POST['fecha_fin'])) {
+                $fecha_inicio = $_POST['fecha_inicio'];
+                $fecha_fin = $_POST['fecha_fin'];
+
+                // Obtener los gastos del usuario dentro del rango de fechas especificado
+                $sql = "SELECT * FROM gastos WHERE username = '$username' AND fecha >= '$fecha_inicio' AND fecha <= '$fecha_fin'";
+            } else {
+                // Obtener los gastos del usuario para el mes actual
+                $current_month = date('Y-m');
+                $sql = "SELECT * FROM gastos WHERE username = '$username' AND DATE_FORMAT(fecha, '%Y-%m') = '$current_month'";
+            }
+
+            $result = $conn->query($sql);
+
+            // Cerrar la conexi髇 a la base de datos
+            $conn->close();
+
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    echo '<tr>';
+                    echo '<td>' . $row['id'] . '</td>';
+                    echo '<td>' . $row['categoria'] . '</td>';
+                    echo '<td>' . $row['monto'] . '</td>';
+                    echo '<td>' . $row['fecha'] . '</td>';
+                    echo '</tr>';
+                }
+            } else {
+                echo '<tr><td colspan="4">No se encontraron gastos para el rango de fechas especificado.</td></tr>';
+            }
         ?>
     </table>
-
     <a href="dashboard.php">Volver al Dashboard</a>
 </body>
 </html>
